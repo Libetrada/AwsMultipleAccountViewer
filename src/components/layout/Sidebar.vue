87f52a1b196c6e05adc3df230d62158ac3880c73
xande0812 <xande0812@gmail.com>
2017-10-19 15:32:58 +0900

    adjust style

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
-  aside.menu
+  aside.menu.sidebar
     p.menu-label General
     ul.menu-list
       li
         a Dashboard
 </template>
 
+<style lang="stylus">
+.sidebar
+    width   180px
+    float   left
+    margin  0
+    padding 0
+</style>
